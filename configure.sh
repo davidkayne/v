f4c74555b67d18a0bf308bb8ad70cbcfd917f519
davidkayne <davidkaynedata@gmail.com>
2025-10-21 21:34:53 +0630

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,46 +1,39 @@
 #!/bin/sh
-
-# Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-
-# Remove temporary directory
-rm -rf /tmp/v2ray
-
-# V2Ray new configuration
+set -e
+TMP_DIR=$(mktemp -d)
+curl -L -H "Cache-Control: no-cache" -o "$TMP_DIR/v2ray.zip" https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip "$TMP_DIR/v2ray.zip" -d "$TMP_DIR"
+install -m 755 "$TMP_DIR/v2ray" /usr/local/bin/v2ray
+install -m 755 "$TMP_DIR/v2ctl" /usr/local/bin/v2ctl
+rm -rf "$TMP_DIR"
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
-    "inbounds": [
-        {
-            "port": 443,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "472523ae-a4a7-42d9-92a1-e302ddba9757", 
-                        "flow": "",
-                        "level": 0,
-                        "email": "test@example.org"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "$ID",
+            "flow": "",
+            "level": 0,
+            "email": "test@example.org"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
-
-# Run V2Ray
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
